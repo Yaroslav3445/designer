723b6e664963f52f1ccb6349dce4107e00d9b8d8
Prystash Yaroslav <prystash.yaroslav@gmail.com>
2024-01-21 18:29:08 +0200

    eleven commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-})
+// let soft = document.querySelectorAll'header'
+document.addEventListener('DOMContentLoaded', () => {
+    // Отримання всіх елементів навігації
+    var navLinks = document.querySelectorAll('.header__href');
+
+    // Додавання обробників подій для кожного елемента навігації
+    navLinks.forEach((navLink) => {
+        navLink.addEventListener('click', function (e)  {
+            // Відміна стандартної дії по кліку на посилання
+
+            navLinks.forEach((link) => {
+                link.classList.remove('class-active');
+            });
+
+            this.classList.add('class-active');
+        });
+    });
+});
